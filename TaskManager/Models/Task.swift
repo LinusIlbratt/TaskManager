ea6f20875d96b8ac30299eb2b1e3de690039d0d3
Andreas Selguson <github@selguson.se>
2024-05-18 10:30:57 +0200

    Added firebase, created login/signup view

--- TaskManager/Models/Task.swift
+++ TaskManager/Models/Task.swift
@@ -6,7 +6,17 @@
 //
 
 import Foundation
+import FirebaseFirestoreSwift
 
-struct Task {
-    
+struct Task: Identifiable, Codable {
+    @DocumentID var id: String?
+    var title: String
+    var description: String
+    var dueDate: Date
+    var isCompleted: Bool
+    var assignedTo: String
+    var createdBy: String
+    var createdAt: Date
+    var familyId: String?
+    var taskColor: String?
 }
