c229c4c3a0ef41d773a02e057ebf05efba33e26c
Linus Ilbratt <linusilbratt@gmail.com>
2024-05-21 10:22:47 +0200

    Created a Profileview with an logout button

---
+++ TaskManager/Views/ProfileView.swift
@@ -0,0 +1,55 @@
+//
+//  ProfileView.swift
+//  TaskManager
+//
+//  Created by Linus Ilbratt on 2024-05-21.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+struct ProfileView: View {
+@Binding var signedIn : Bool
+
+var body: some View {
+    ZStack {
+        Color(UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1))
+            .edgesIgnoringSafeArea(.all)
+        
+        VStack {
+            Spacer()
+            
+            Button(action: {
+                do {
+                    try Auth.auth().signOut()
+                    signedIn = false
+                } catch let signOutError as NSError {
+                    print("Error signing out: \(signOutError)")
+                }
+            }) {
+                Text("Logout")
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.clear)
+                    .foregroundColor(.black)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 1)
+                    )
+            }
+            .padding(.horizontal, 20)
+            .padding(.bottom, 40)
+        }
+    }
+}
+}
+
+
+struct ProfileView_Previews: PreviewProvider {
+    @State static var signedIn = false
+
+    static var previews: some View {
+        ProfileView(signedIn: $signedIn)
+    }
+}
