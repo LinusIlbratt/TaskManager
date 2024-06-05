6544525e89583742fc4e6429fb37bf2e1a258b49
Linus Ilbratt <linusilbratt@gmail.com>
2024-06-05 08:09:18 +0200

    added elements to display user displayname and amount of fishes

--- TaskManager/Views/ProfileView.swift
+++ TaskManager/Views/ProfileView.swift
@@ -9,31 +9,42 @@ import SwiftUI
 import FirebaseAuth
 
 struct ProfileView: View {
-@Binding var signedIn : Bool
-@State private var navigateToNewGroup = false
-@State private var navigateToHireService = false
+    @Binding var signedIn: Bool
+    @State private var navigateToNewGroup = false
+    @State private var navigateToHireService = false
+    @StateObject private var userViewModel = UserViewModel()
     
-
-var body: some View {
+    var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            Text("Manage Groups")
-                .font(.title)
-                .bold()
-                .padding(.bottom, 20)
-                .padding(.horizontal, 20)
-                        
-            //List groups you are member of
+            
+            // Display the user's displayName
+            HStack {
+                Spacer()
+                VStack {
+                    Text("\(userViewModel.currentUser?.displayName ?? "")")
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                        .padding(.horizontal, 20)
+                        .padding(.top, 5)
+                    
+                    Text("Total Mörtar")
+                        .foregroundColor(.gray)
+                        .padding(.top, 5)
+                        .font(.system(size: 24))
+                              
+                    TotalAmountOfFishesCollectedView(totalAmountOfFishesCollected: userViewModel.totalAmountOfFishesCollected)
+                }
+                Spacer()
+            }
             
             
             Spacer()
-            
             
             Button(action: {
                 //Manage family
                 navigateToNewGroup = true
             }) {
                 Text("Create New Group")
-                
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity)
                     .padding()
@@ -43,12 +54,10 @@ var body: some View {
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(Color.black, lineWidth: 1)
                     )
-                
             }
             .navigationDestination(isPresented: $navigateToNewGroup) {
                 NewGroupView(signedIn: $signedIn, groupName: "", description: "")
             }
-                                      
             .padding(.horizontal, 20)
             
             Button(action: {
@@ -64,7 +73,6 @@ var body: some View {
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(Color.black, lineWidth: 1)
                     )
-
             })
             .navigationDestination(isPresented: $navigateToHireService) {
                 HireServiceView()
@@ -93,13 +101,36 @@ var body: some View {
             .padding(.horizontal, 20)
             .padding(.bottom, 40)
         }
+        .onAppear {
+            userViewModel.fetchCurrentUser()
+            userViewModel.fetchCurrentUserTotalAmountOfFishesCollected()
+        }
+    }
+}
+
+struct TotalAmountOfFishesCollectedView: View {
+    var totalAmountOfFishesCollected: Int?
+
+    var body: some View {
+        Group {
+            if let totalAmountOfFishesCollected = totalAmountOfFishesCollected {
+                Text("\(totalAmountOfFishesCollected)")
+                    .font(.system(size: 40, weight: .bold))
+                    .foregroundColor(.gray)
+                    
+            } else {
+                Text("Loading...")
+                    .font(.system(size: 30, weight: .bold))
+                    .foregroundColor(.gray)
+            }
+        }
     }
 }
 
 
 struct ProfileView_Previews: PreviewProvider {
     @State static var signedIn = false
-
+    
     static var previews: some View {
         ProfileView(signedIn: $signedIn)
     }
